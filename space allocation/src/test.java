ada8fe4dbadc09fa164860d14090f1acdcd3a373
Tondra <32168789+MeheroonTondra@users.noreply.github.com>
2017-12-01 10:41:53 -0330

    Add files via upload

---
+++ space allocation/src/test.java
@@ -0,0 +1,22 @@
+
+public class test {
+
+if(!(Sch.getR5().getD1().getAv())) {
+	day1_4.setEnabled(false);
+}
+if(!(Sch.getR5().getD2().getAv())) {
+	day2_4.setEnabled(false);
+}
+if(!(Sch.getR5().getD3().getAv())) {
+	day3_4.setEnabled(false);
+}
+if(!(Sch.getR5().getD4().getAv())) {
+	day4_4.setEnabled(false);
+}
+if(!(Sch.getR5().getD5().getAv())) {
+	day5_4.setEnabled(false);
+}
+if(!(Sch.getR5().getD1().getT1())) {
+	mtime1_4.setEnabled(false);
+	
+}
